1b6c326ed22eb38291961f42b15acf9afaaa99a8
insinger <insinger@entpsystems.com>
2017-06-14 16:50:47 +0800

    fix bug

--- start.js
+++ start.js
@@ -1,13 +1,6 @@
-// i'm changing this file
 var express=require("express")
 var app=express()
 
-function chris(req,res,next) {
-	console.log("req=",req)
-	console.log("res=",res)
-	console.log("next=",next)
-}
-// app.use(express.static(__dirname+"/public"))
-app.use(chris)
+app.use(express.static(__dirname+"/public"))
 app.use("/images",express.static(__dirname+"/public/pics"))
 app.listen(3000,function () {console.log("listening on port 3000");})
